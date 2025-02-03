6f25392b18bafb8947ab3c0cb2452cbb3138f851
Muhammad-Aman171 <ag3734359@gmail.com>
2025-02-03 14:45:20 +0500

    E-commerce

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,22 @@ module.exports = {
       colors: {
         primary: "#EDA415",
         textColor: "#1B5A7D",
+        secoundTextColor: "#003F62",
+        thirdTextColor: "#232323",
+        forthTextColor: "#434343",
+        fifthTextColor: "#4A4A4A",
+
         borderColor: "#ADADAD",
+        secoundBorderColor: "#BDBDBD",
+        thirdBorderColor: "#B6B6B6",
+        forthBorderColor: "#BABABA",
+
+        cartProductCardTextColor: "#2F2F2F",
+        navbarLinkTextColor: "#3A3A3A",
+        latestNewsBorderColor: "#C8C8C8",
+        footerSectionTextColor: "#D9D9D9",
+        ProductDetailsBgColor: "#EEEEEE",
+        allCategroriesTextColor: "#595959",
       },
       screens: {
         xs: "380px",
